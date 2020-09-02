b1843d41c456eec32d834cfb727bc52f7ae72a9f
Promise Fru <promisefru1@gmail.com>
2020-09-02 17:11:21 +0100

    person prototype created

--- myApp.js
+++ myApp.js
@@ -40,9 +40,16 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // fields, use simple validators like `required` or `unique`, and set
 // `default` values. See the [mongoose docs](http://mongoosejs.com/docs/guide.html).
 
-// <Your code here >
+var Schema = mongoose.Schema;
 
-var Person /* = <Your Model> */
+var personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+})
+
+
+var Person = mongoose.model("Person", personSchema);
 
 // **Note**: Glitch is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
